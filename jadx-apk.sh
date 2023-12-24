32c0af65cad2c3fe9903a4c55da55c95eb47d00e
Kristina Valentei <140957188+klssstis@users.noreply.github.com>
2023-12-24 17:54:44 +0300

    Update jadx-apk.sh

--- jadx-apk.sh
+++ jadx-apk.sh
@@ -2,19 +2,14 @@
 SCRIPT_DIR="$(dirname "$0")"
 export SCRIPT_DIR
 
-# 校验参数合法性
 source $SCRIPT_DIR/util.sh
 check $1
 
-# 得到新的目录
 newfoldername=${foldername}"-jadx"
 rm -rf $newfoldername
 
-# 通过jadx-gui查看apk文件
 echo "View the decompile file"
 
-# 执行jadx命令
 #sh $SCRIPT_DIR/tools/jadx/bin/jadx -d $newfoldername $absolutename
 
-# 执行jadx-gui命令
 sh $SCRIPT_DIR/tools/jadx/bin/jadx-gui $absolutename
