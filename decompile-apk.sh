c5c019700a0f892e0c787d798d55821b109c4fe0
Kristina Valentei <140957188+klssstis@users.noreply.github.com>
2023-12-24 17:54:08 +0300

    Update decompile-apk.sh

--- decompile-apk.sh
+++ decompile-apk.sh
@@ -5,7 +5,6 @@ export SCRIPT_DIR
 arg=$1
 
 function input() {
-	# true 显示以下信息，false不显示
 	if [ $1 = true ]; 
 		then
 		echo "You can choose the following three ways to decompile an apk file"
@@ -16,32 +15,25 @@ function input() {
 		echo "q: exit"
 	fi
 
-	#echo "Please input your choice(1, 2, 3, 4):"
 	read -p "Please input your choice(1, 2, 3, 4):" input
 	
 	case $input in
 		1)
-		## 执行jdgui-apk命令
-		sh $SCRIPT_DIR/jdgui-apk.sh $arg
+		bash $SCRIPT_DIR/jdgui-apk.sh $arg
 		;;
 		2)
-		## 执行classyshark-apk命令
-		sh $SCRIPT_DIR/classyshark-apk.sh $arg
+		bash $SCRIPT_DIR/classyshark-apk.sh $arg
 		;;
 		3)
-		## 执行jadx-apk命令
-		sh $SCRIPT_DIR/jadx-apk.sh $arg
+		bash $SCRIPT_DIR/jadx-apk.sh $arg
 		;;
 		4)
-		## 执行bytecode-viewer-apk命令
-		sh bytecode-viewer.sh $arg
+		bash bytecode-viewer.sh $arg
 		;;
 		q)
-		## 退出命令
 		exit
 		;;
 		*)
-		## 输入其他字符重新选择
 		input false
 		;;
 	esac
