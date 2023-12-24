f35964167d5b954bd73b8e650e6b65c0f9d9e259
Kristina Valentei <140957188+klssstis@users.noreply.github.com>
2023-12-24 17:53:19 +0300

    Update classyshark-apk.sh

--- classyshark-apk.sh
+++ classyshark-apk.sh
@@ -2,11 +2,9 @@
 SCRIPT_DIR="$(dirname "$0")"
 export SCRIPT_DIR
 
-# 校验参数合法性
 source $SCRIPT_DIR/util.sh
 check $1
 
-# 得到新的目录
 newfoldername=${foldername}"-classyshark"
 rm -rf $foldername
 
@@ -15,15 +13,11 @@ if [ ! -d "$newfoldername" ];
 	mkdir $newfoldername
 fi
 
-# 切换到创建的目录
 cd $newfoldername
 
-# 打印该目录的路径
 echo `pwd`
 
-# 通过ClassyShark查看apk文件
 echo "View the decompile file"
 
-# 执行ClassyShark命令
-#java -jar $SCRIPT_DIR/tools/classyshark/ClassyShark.jar -export $absolutename
-java -jar $SCRIPT_DIR/tools/classyshark/ClassyShark.jar -open $absolutename
+java -jar $SCRIPT_DIR/tools/classyshark/ClassyShark.jar -export $absolutename
+#java -jar $SCRIPT_DIR/tools/classyshark/ClassyShark.jar -open $absolutename
